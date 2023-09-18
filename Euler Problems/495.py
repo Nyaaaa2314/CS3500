f2ee01294fa03b34ff7a859483be2431527953d5
Jay <Jadiel.r25@gmail.com>
2023-09-18 10:10:28 -0400

    work

--- Euler Problems/495.py
+++ Euler Problems/495.py
@@ -15,6 +15,10 @@
 """
 import sys
 import itertools
+import resource
+import numpy as np
+from scipy.special import comb
+import math
 
 global maxfactor
 global primes
@@ -22,10 +26,13 @@ global n
 global k
 global total
 def factorial(n):
-    if n == 1:
-        return 1
-    else:
-        return n * factorial(n - 1)
+    return math.factorial(n)
+
+def nfactorial(n, m):
+    sum = 1
+    for i in range(n, n +m):
+        sum *= i
+    return sum
 def initPrimes():
     nums = open("primes-million.txt").read().split()
     for i in nums:
@@ -35,28 +42,39 @@ def processInput(s):
         return int(s)
     except:
         try:
+            #print(int(s.split("!")[0]))
             return factorial(int(s.split("!")[0]))
         except:
             return ValueError
         
-def makeLists(lst, rt, size):
+def makeLists(lst, rt, curi, size):
     if size == k:
         if rt == n:
+            global total
             total += 1
         return
     else:
-        for i in range(maxfactor + 1):
-            if(i * rt > n):
+        for i in range(curi, maxfactor + 1):
+            if(i * rt > n or nfactorial(i, k - size) > n):
                 return
             else:
                 lst.append(i)
-                makeLists(lst, rt * i, size + 1)
+                makeLists(lst, rt * i, i + 1, size + 1)
                 lst.pop()
 
+
+
+
+
+def limit_memory(maxsize):
+    soft, hard = resource.getrlimit(resource.RLIMIT_AS)
+    resource.setrlimit(resource.RLIMIT_AS, (maxsize, hard))
 
 sys.set_int_max_str_digits(100000)
 
 #Initializing variables
+total = 0
+limit_memory(10000000000)
 primes = set()
 initPrimes()
 n = processInput(input("Enter n: "))
@@ -71,22 +89,35 @@ if factorial(k) > n:
 if factorial(k) == n:
     print(1)
     exit() 
-nums = list(range(1, maxfactor + 1))
-
+#nums = list(range(1, maxfactor + 1))
 valid = 0
-#combinations = list(itertools.combinations(nums, k))
-#print(combinations)
-repeats = set()
-for i in combinations:
-    total = 1
-    for j in i:
-        total *= j
-        if total > n:
-            break
-    if total == n:
-        valid += 1
-    # if total > n:
-    #     break
-print(valid)
+sum = 0
+start = list()
+makeLists(start, 1, 1, 0)
+valid = total
+# inv = list()
+# pairs = list()
+# for i in range(1, maxfactor):
+#     inv.append(factorial(maxfactor - i))
+# itr = 1
+# for i in inv:
+#     sum += i 
+#     pairs.append((itr, i))
+#     itr += 1
+# for i in pairs:
+#     print(str(i[0]) + " " + str(i[1]) + " percentage of total combs: " + str(i[1] / sum))
+#print(comb(nparr, k, exact=True) )
+# combinations = itertools.combinations(nums, k)
+# for i in combinations:
+#     total = 1
+#     for j in i:
+#         total *= j
+#         if total > n:
+#             break
+#     if total == n:
+#         valid += 1
+
+print(valid % MOD)
+#print(str(nfactorial(22, 3)) + " " + str(24 *23 * 22))
 
 
